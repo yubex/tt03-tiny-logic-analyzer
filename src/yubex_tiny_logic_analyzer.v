13a8f32fd521eac9089523f3f26acfd173ebcbab
yubex <118074126+yubex@users.noreply.github.com>
2023-04-02 15:32:58 +0200

    Update yubex_tiny_logic_analyzer.v

--- src/yubex_tiny_logic_analyzer.v
+++ src/yubex_tiny_logic_analyzer.v
@@ -33,7 +33,7 @@ module yubex_tiny_logic_analyzer (
   always @(posedge clk or posedge rst)
   begin
 	if(rst) begin
-        	    sample_sr <= {sample_sr_size{1'b0}};
+       		sample_sr <= {sample_sr_size{1'b0}};
     	    end 
     else begin
         sample_sr <= {sample_sr[sample_sr_size-2:0],data_in};
@@ -46,8 +46,8 @@ module yubex_tiny_logic_analyzer (
 	if(rst) begin
                 high_detected         <= 1'b0;
                 low_detected          <= 1'b0;
-                rising_edge_detected  <= 1'b0;
-                falling_edge_detected <= 1'b0;
+                rising_edge_detected  <= 1'b1;
+                falling_edge_detected <= 1'b1;
     	    end 
     else begin
         high_detected         <= 1'b0;
@@ -65,11 +65,11 @@ module yubex_tiny_logic_analyzer (
                   end
               2'b01: 
                   begin
-	                rising_edge_detected <= 1'b1;
+	            rising_edge_detected <= 1'b1;
                   end
               2'b10: 
                   begin
-	                falling_edge_detected <= 1'b1;
+	            falling_edge_detected <= 1'b1;
                   end
               default:;
           endcase
@@ -77,18 +77,18 @@ module yubex_tiny_logic_analyzer (
   end
 
   // add sr for edge events, to make it visible on the 7 segment display
-  always @(posedge clk or posedge rst or posedge rising_edge_detected )
+  always @(posedge clk or posedge rising_edge_detected )
   begin
-	if(rising_edge_detected || rst) begin
+	if(rising_edge_detected) begin
         	    rising_edge_sr <= {event_sr_size{1'b1}};
     	    end 
     else begin
         rising_edge_sr <= {rising_edge_sr[event_sr_size-2:0],1'b0};
     end
   end 
-  always @(posedge clk or posedge rst or posedge falling_edge_detected)
+  always @(posedge clk or posedge falling_edge_detected)
   begin
-	if(falling_edge_detected || rst) begin
+	if(falling_edge_detected) begin
         	    falling_edge_sr <= {event_sr_size{1'b1}};
     	    end 
     else begin
